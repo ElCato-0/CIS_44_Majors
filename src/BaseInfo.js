9bdab3c0de6144169d5f77469b2565a149c28a21
Saad5400 <sdbtwa@gmail.com>
2023-06-22 04:54:53 +0300

    aaa

--- src/BaseInfo.js
+++ src/BaseInfo.js
@@ -1,74 +1,91 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 function FullScreenImage({ src, alt, thumbnailClass, fullScreenClass }) {
-  const [isFullScreen, setIsFullScreen] = useState(false);
 
-  const handleImageClick = () => {
-    setIsFullScreen(true);
-  };
+    const handleImageClick = () => {
+        // open a new window with the image
+        window.open(src, '_blank');
+    };
 
-  const handleFullScreenClose = () => {
-    setIsFullScreen(false);
-  };
-
-  return (
-    <div className="relative z-10">
-      <div
-        className={`w-64 h-64 md:w-80 md:h-80 bg-gray-400 cursor-pointer ${thumbnailClass}`}
-        style={{ backgroundImage: `url('${src}')`, backgroundSize: 'cover' }}
-        onClick={handleImageClick}
-      />
-      {isFullScreen && (
-        <div
-          className={`fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-75 flex justify-center items-center ${fullScreenClass}`}
-          onClick={handleFullScreenClose}
-        >
-          <img
-            src={src}
-            alt={alt}
-            className="max-h-full max-w-full"
-          />
+    return (
+        <div className="relative z-10">
+            <div
+                className={`w-64 h-64 md:w-80 md:h-80 bg-gray-400 cursor-pointer ${thumbnailClass}`}
+                style={{ backgroundImage: `url('${src}')`, backgroundSize: 'cover' }}
+                onClick={handleImageClick}
+            />
         </div>
-      )}
-    </div>
-  );
+    );
 }
 
 export default function BaseInfo(props) {
-    return (
-        <article className='prose'>
-            <h1>الخطة</h1>
-            <p>
-                اضغط على الصورة لتكبيرها
-            </p>
-            <br />
-            {/* display an image */}
-            <div className="join join-vertical md:join-horizontal mx-auto">
-                <FullScreenImage src={props.image1} class="join-item object-cover h-48 w-96" />
-                <img src={props.image2} class="join-item object-cover h-48 w-96" />
-            </div>
 
-            <br />
-            <br />
-            <h1>
-                المقررات
-            </h1>
-            <br />
-            <br />
+    const location = useLocation();
+    const [header, setHeader] = useState('');
+
+    useEffect(() => {
+        let routesNames = {
+            '/': 'الرئيسية',
+            '/cs': 'علوم الحاسب',
+            '/ce': 'هندسة الحاسب',
+            '/se': 'هندسة البرمجيات',
+            '/sec': 'الامن السيبراني',
+            '/ds': 'علم البيانات',
+            '/ai': 'الذكاء الاصطناعي',
+            '/hci': 'تفاعل المستخدم مع الحاسب',
+        }
+        setHeader(routesNames[location.pathname]);
+    }, [location]);
+
+    return (
+        <>
+            <article className='prose'>
+                <h1>{header}</h1>
+                <hr />
+                <h2>الخطة</h2>
+                <p>
+                    اضغط على الصورة لفتحها
+                </p>
+
+                {/* make a flex that is centred and has a horizontal flow */}
+                <div className="flex justify-center flex-wrap">
+                    {/* make a div that is 1/2 of the width of the parent */}
+                    <div className="w-2/5">
+                        <FullScreenImage src={props.image1} class="join-item object-cover h-48 w-96" />
+                    </div>
+                    <div className='w-1/5'>
+
+                    </div>
+                    {/* make a div that is 1/2 of the width of the parent */}
+                    <div className="w-2/5">
+                        <FullScreenImage src={props.image2} class="join-item object-cover h-48 w-96" />
+                    </div>
+                </div>
+
+                <h2>
+                    المقررات
+                </h2>
+            </article>
             <div className="overflow-x-auto">
                 <table className="table">
                     {/* head */}
                     <thead>
                         <tr>
-                            <th className="text-right">المقرر</th>
-                            <th className="text-right">الوصف التقريبي</th>
+                            <th className="text-right text-xl">المقرر</th>
+                            <th className="text-right text-xl">
+                                الوصف&nbsp;
+                                <strong className='text-error'>
+                                    التقريبي
+                                </strong>
+                            </th>
                         </tr>
                     </thead>
                     <tbody>
                         {/* loop for each row in props.data and display a tr */}
                         {props.data.map((row, index) => {
                             return (
-                                <tr key={index}>
+                                <tr key={index} className='text-xl'>
                                     {/* loop for each column in the row and display a td */}
                                     {row.map((column, index) => {
                                         return (
@@ -81,6 +98,8 @@ export default function BaseInfo(props) {
                     </tbody>
                 </table>
             </div>
-        </article>
+        </>
+
+
     );
 }
--- src/index.js
+++ src/index.js
@@ -27,6 +27,60 @@ root.render(
               ]}
             />}
           />
+          <Route path="/ce" element={
+            <BaseInfo
+              image1='https://drive.uqu.edu.sa/_/cis/images/New%20programs/%D8%A8%D9%83%D8%A7%D9%84%D9%88%D8%B1%D9%8A%D9%88%D8%B3%20%D9%87%D9%86%D8%AF%D8%B3%D8%A9%20%D8%A7%D9%84%D8%AD%D8%A7%D8%B3%D8%A8%20%D8%A7%D9%84%D8%A2%D9%84%D9%8A%20%D9%88%D8%A7%D9%84%D8%B4%D8%A8%D9%83%D8%A7%D8%AA.jpg'
+              image2='https://drive.uqu.edu.sa/_/cis/images/New%20programs/%D8%AE%D8%B7%D8%A9%20%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D8%AC%20%D9%87%D9%86%D8%AF%D8%B3%D8%A9%D8%A7%D9%84%D8%AD%D8%A7%D8%B3%D8%A8%20%D8%A7%D9%84%D8%A2%D9%84%D9%8A%20%D9%88%D8%A7%D9%84%D8%B4%D8%A8%D9%83%D8%A7%D8%AA.jpg'
+              data={[
+                ['name', 'des'],
+              ]}
+            />}
+          />
+          <Route path="/se" element={
+            <BaseInfo
+              image1='https://drive.uqu.edu.sa/_/cis/images/New%20programs/%D8%A8%D9%83%D8%A7%D9%84%D9%88%D8%B1%D9%8A%D9%88%D8%B3%20%D9%87%D9%86%D8%AF%D8%B3%D8%A9%20%D8%A7%D9%84%D8%A8%D8%B1%D9%85%D8%AC%D9%8A%D8%A7%D8%AA.jpg'
+              image2='https://drive.uqu.edu.sa/_/cis/images/New%20programs/%D8%AE%D8%B7%D8%A9%20%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D8%AC%20%D9%87%D9%86%D8%AF%D8%B3%D8%A9%20%D8%A7%D9%84%D8%A8%D8%B1%D9%85%D8%AC%D9%8A%D8%A7%D8%AA.jpg'
+              data={[
+                ['name', 'des'],
+              ]}
+            />}
+          />
+          <Route path="/sec" element={
+            <BaseInfo
+              image1='https://drive.uqu.edu.sa/_/cis/images/New%20programs/%D8%A8%D9%83%D8%A7%D9%84%D9%88%D8%B1%D9%8A%D9%88%D8%B3%20%D8%A7%D9%84%D8%A3%D9%85%D9%86%20%D8%A7%D9%84%D8%B3%D9%8A%D8%A8%D8%B1%D8%A7%D9%86%D9%8A.jpg'
+              image2='https://drive.uqu.edu.sa/_/cis/images/New%20programs/%D8%AE%D8%B7%D8%A9%20%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D8%AC%20%D8%A7%D9%84%D8%A3%D9%85%D9%86%20%D8%A7%D9%84%D8%B3%D9%8A%D8%A8%D8%B1%D8%A7%D9%86%D9%8A.jpg'
+              data={[
+                ['name', 'des'],
+              ]}
+            />}
+          />
+          <Route path="/ds" element={
+            <BaseInfo
+              image1='https://drive.uqu.edu.sa/_/cis/images/New%20programs/%D8%A8%D9%83%D8%A7%D9%84%D9%88%D8%B1%D9%8A%D9%88%D8%B3%20%D8%B9%D9%84%D9%85%20%D8%A7%D9%84%D8%A8%D9%8A%D8%A7%D9%86%D8%A7%D8%AA.jpg'
+              image2='https://drive.uqu.edu.sa/_/cis/images/New%20programs/%D8%AE%D8%B7%D8%A9%20%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D8%AC%20%D8%B9%D9%84%D9%85%20%D8%A7%D9%84%D8%A8%D9%8A%D8%A7%D9%86%D8%A7%D8%AA.jpg'
+              data={[
+                ['name', 'des'],
+              ]}
+            />}
+          />
+          <Route path="/ai" element={
+            <BaseInfo
+              image1='https://drive.uqu.edu.sa/_/cis/images/New%20programs/%D8%A8%D9%83%D8%A7%D9%84%D9%88%D8%B1%D9%8A%D9%88%D8%B3%20%D8%A7%D9%84%D8%B0%D9%83%D8%A7%D8%A1%20%D8%A7%D9%84%D8%A7%D8%B5%D8%B7%D9%86%D8%A7%D8%B9%D9%8A.jpg'
+              image2='https://drive.uqu.edu.sa/_/cis/images/New%20programs/%D8%AE%D8%B7%D8%A9%20%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D8%AC%20%D8%A7%D9%84%D8%B0%D9%83%D8%A7%D8%A1%20%D8%A7%D9%84%D8%A7%D8%B5%D8%B7%D9%86%D8%A7%D8%B9%D9%8A.jpg'
+              data={[
+                ['name', 'des'],
+              ]}
+            />}
+          />
+          <Route path="/hci" element={
+            <BaseInfo
+              image1='https://drive.uqu.edu.sa/_/cis/images/New%20programs/%D8%A8%D9%83%D8%A7%D9%84%D9%88%D8%B1%D9%8A%D9%88%D8%B3%20%D8%AA%D9%81%D8%A7%D8%B9%D9%84%20%D8%A7%D9%84%D8%A5%D9%86%D8%B3%D8%A7%D9%86%20%D9%85%D8%B9%20%D8%A7%D9%84%D8%AD%D8%A7%D8%B3%D8%A8.jpg'
+              image2='https://drive.uqu.edu.sa/_/cis/images/New%20programs/%D8%AE%D8%B7%D8%A9%20%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D8%AC%20%D8%AA%D9%81%D8%A7%D8%B9%D9%84%20%D8%A7%D9%84%D8%A5%D9%86%D8%B3%D8%A7%D9%86%20%D9%85%D8%B9%20%D8%A7%D9%84%D8%AD%D8%A7%D8%B3%D8%A8.jpg'
+              data={[
+                ['name', 'des'],
+              ]}
+            />}
+          />
         </Routes>
       </App>
     </BrowserRouter>
