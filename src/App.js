c8bfc8eb3825093d0e3f2fdba9f78d72ecb658d2
Saad5400 <sdbtwa@gmail.com>
2023-06-22 03:33:31 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import csImage from './images/cs.jpg';
+import ceImage from './images/ce.jpg';
+import seImage from './images/se.jpg';
+import secImage from './images/sec.jpg';
+import dsImage from './images/ds.jpg';
+import aiImage from './images/ai.jpg';
+import hciImage from './images/hci.jpg';
+
+
+function Navbar(props) {
+    return (
+        <div className="navbar bg-base-300">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost btn-circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a>Homepage</a></li>
+                        <li><a>Portfolio</a></li>
+                        <li><a>About</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="navbar-end">
+                <a className="btn btn-ghost normal-case text-xl">تخصصات الحاسب د44</a>
+            </div>
+        </div>
+    );
+}
+
+function Card(props) {
+    return (
+        <div className={"card card-compact w-80 md:w-96 bg-base-100 shadow-xl mx-auto " + props.className}>
+            <figure><img src={props.image} loading='lazy' alt={props.title} /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{props.title}</h2>
+                <p>{props.description}</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">معلومات اكثر</button>
+                </div>
+            </div>
+        </div>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='arabic-font bg-black min-h-screen'>
+            <Navbar />
+            <br />
+            <br />
+            <br />
+            <div className='container mx-auto'>
+                <div dir='rtl' className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4'>
+                    <Card title="علوم الحاسب" image={csImage} description='يتميز بتقديم مجموعة من المقررات الدراسية التي تغطي مجالات علوم الحاسب الأساسية والتطبيقية والتي تمكن الطالب من الحصول على معرفة واسعة ومتميزة في مجال علوم الحاسب وتمكنه من العمل في مجالات الحاسب المختلفة.' />
+                    <Card title="هندسة الحاسب" image={ceImage} />
+                    <Card title="هندسة البرمجيات" image={seImage} />
+                    <Card title="امن السيبراني" image={secImage} />
+                    <Card title="علم البيانات" image={dsImage} />
+                    <Card title="ذكاء اصطناعي" image={aiImage} />
+                    <Card title="تفاعل المستخدم مع الحاسب" className='md:col-span-2 xl:col-span-3' image={hciImage} />
+                </div>
+            </div>
+            <br />
+            <br />
+            <br />
+        </div>
+    );
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#ff75f5",
+          "secondary": "#156b75",
+          "accent": "#6fe87d",
+          "neutral": "#342636",
+          "base-100": "#2f3a42",
+          "info": "#68c4ee",
+          "success": "#0f6259",
+          "warning": "#f2c769",
+          "error": "#f77459",
+        },
+      },
+    ],
+  },
+}
+
